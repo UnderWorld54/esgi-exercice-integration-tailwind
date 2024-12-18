234be2234f5032185ca209c0cca7f6588dcbde9c
SAMINA Angelo <angelosamina@gmail.com>
2024-12-18 11:45:40 +0100

    Mise en place de la navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["index.html", "src/**/*.{css,js,jsx,ts,tsx}"],
   theme: {
+    colors : {
+      'primary': {
+        light: '#6533FF',
+        DEFAULT: '#6533FF',
+        dark: '#6533FF',
+      },
+    },
     extend: {},
   },
   plugins: [],
