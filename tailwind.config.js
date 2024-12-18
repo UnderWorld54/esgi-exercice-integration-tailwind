a25f61105a45ac5f581f662f9d44d780fd3172ba
SAMINA Angelo <angelosamina@gmail.com>
2024-12-18 12:50:46 +0100

    Mise en place des sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@
 module.exports = {
   content: ["index.html", "src/**/*.{css,js,jsx,ts,tsx}"],
   theme: {
-    colors : {
+    colors: {
       'primary': {
         light: '#6533FF',
         DEFAULT: '#6533FF',
         dark: '#6533FF',
+      },
+      'secondary': {
+        light: '#FBE4AB',
+        DEFAULT: '#FBE4AB',
+        dark: '#FBE4AB',
+      },
+      'neutral': {
+        light: '#272727',
+        DEFAULT: '#272727',
+        dark: '#272727',
       },
     },
     extend: {},
